1b80352ec43daef92f4f173c5accb6b7ae0d56b6
Terje Urnes <terje@urnes.net>
2022-02-16 23:48:06 +0100

    Adding script file

---
+++ palette.js
@@ -0,0 +1,17 @@
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === 'interactive') {
+        initPalette();
+    }
+});
+
+window.addEventListener('resize', function () {
+
+});
+
+var t79P = {
+
+}
+
+function initPalette() {
+
+}
